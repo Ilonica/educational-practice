8ec8abd636a8d041706f293b3ee3ada09018b006
Ilonica <95592035+Ilonica@users.noreply.github.com>
2023-07-02 03:01:17 +0300

    Add files via upload

---
+++ educational practice 2022/Hamming code.cpp
@@ -0,0 +1,182 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    setlocale(0, "");
+    fstream f("posled.txt");
+    int r;
+
+    cout << "Программа кодирования и декодирования заданных двоичных последовательностей.\nСоздайте файл с именем posled.txt и укажите в нем номер команды\nи значение двоичной последовательности через пробел.\n\n";
+        if (!f.is_open()) cout << "Файл не найден!\n";
+    if (f.is_open() && f.peek() == EOF) cout << "Файл пустой!\n";
+
+    if (f.is_open() && f.peek() != EOF) {
+        f >> r;
+        if (f.fail()) cout << "Данные введены неверно!\n";
+
+        else {
+            cout << "Вы ввели номер команды " << r;
+            if (r == 1) {
+                cout << " - закодировать.\n";
+                int step = 0, t = 0, sum = 0, j = 0, p = 0;
+                const int n = 26;
+                int a[n], b[n + 5];
+                for (int i = 0; i < n; i++)
+                    f >> a[i];
+                if (f.fail()) cout << "Данные введены неверно!\n";
+
+                else {
+                    f.close();
+                    cout << "Вы ввели двоичную последовательность:\n";
+                    for (int i = 0; i < n; i++)
+                        cout << a[i];
+                    cout << endl << endl;
+
+                    for (int i = 0; i < n + 5; i++) {
+                        if (i == (pow(2, step) - 1)) {
+                            b[i] = 0;
+                            step++;
+                        }
+                        else {
+                            b[i] = a[t];
+                            t++;
+                        }
+                    }
+
+                    cout << "Двоичная последовательность с контрольными битами, равными нулю:\n";
+                    for (int i = 0; i < n + 5; i++)
+                        cout << b[i];
+                    cout << endl << endl;
+
+                    step = 0;
+                    for (int i = 0; i < n + 5; i++) {
+                        if (i == (pow(2, step) - 1)) {
+                            j = i;
+                            while (j < n + 5) {
+                                if (p < pow(2, step)) {
+                                    sum += b[j];
+                                    j++; p++;
+                                }
+                                else {
+                                    j += (pow(2, step));
+                                    p = 0;
+                                }
+                            }
+                            if (sum % 2 == 0) b[i] = 0;
+                            else b[i] = 1;
+                            p = 0;
+                            sum = 0;
+                            step++;
+                        }
+                    }
+
+                    cout << "Закодированная двоичная последовательность:\n";
+                    for (int i = 0; i < n + 5; i++)
+                        cout << b[i];
+                    cout << endl;
+
+                    ofstream v("kod.txt");
+                    for (int i = 0; i < n + 5; i++)
+                        v << b[i] << " ";
+                    v.close();
+                }
+            }
+
+
+            if (r == 2) {
+                cout << " - декодировать.\n";
+                int step = 0, t = 0, sum = 0, j = 0, p = 0, kontrol = 0, iter = 0;
+                const int n = 31;
+                int a[n], b[n], c[n - 5];
+                for (int i = 0; i < n; i++) {
+                    f >> a[i];
+                    b[i] = a[i];
+                }
+                if (f.fail()) cout << "Данные введены неверно!\n";
+
+                else {
+                    f.close();
+
+                    cout << "Вы ввели двоичную последовательность:\n";
+                    for (int i = 0; i < n; i++)
+                        cout << a[i];
+                    cout << endl << endl;
+
+                    for (int i = 0; i < n; i++) {
+                        if (i == (pow(2, step) - 1)) {
+                            j = i;
+                            while (j < n) {
+                                if (j == pow(2, step) - 1) {
+                                    j++; p++;
+                                }
+                                else if (p < pow(2, step)) {
+                                    sum += b[j];
+                                    j++; p++;
+                                }
+                                else {
+                                    j += (pow(2, step));
+                                    p = 0;
+                                }
+                            }
+                            if (sum % 2 == 0) b[i] = 0;
+                            else b[i] = 1;
+                            p = 0;
+                            sum = 0;
+                            step++;
+                        }
+                    }
+                    cout << "Двоичная последовательность с пересчитанными контрольными битами:\n";
+                    for (int i = 0; i < n; i++)
+                        cout << b[i];
+                    cout << endl;
+
+                    step = 0;
+                    for (int i = 0; i < n; i++) {
+                        if (i == (pow(2, step) - 1)) {
+                            if (b[i] != a[i]) {
+                                if (i == 0) kontrol++;
+                                else {
+                                    kontrol += i;
+                                    iter++;
+                                }
+                            }
+                            step++;
+                        }
+                    }
+
+                    if ((kontrol + iter) > 0)
+                        cout << "Ошибка в " << (kontrol + iter) << "-ом бите!\n" << endl;
+                    else cout << "Ошибок нет.\n" << endl;
+
+                    for (int i = 0; i < n; i++) {
+                        if (kontrol != 0 && ((kontrol + iter) - 1) == i && b[i] == 0) b[i] = 1;
+                        else if (kontrol != 0 && ((kontrol + iter) - 1) == i && b[i] == 1) b[i] = 0;
+                    }
+                    step = 0;
+                    for (int i = 0; i < n; i++) {
+                        if (i == (pow(2, step) - 1)) {
+                            step++;
+                        }
+                        else {
+                            c[t] = b[i];
+                            t++;
+                        }
+                    }
+                    cout << "Декодированная двоичная последовательность:\n";
+                    for (int i = 0; i < n - 5; i++)
+                        cout << c[i];
+                    cout << endl;
+
+                    ofstream v("kod.txt");
+                    for (int i = 0; i < n - 5; i++)
+                        v << c[i] << " ";
+                    v.close();
+                }
+            }
+
+            if (r < 1 || r > 2) cout << "." << "\nВведен неверный номер команды!\n";
+        }
+    }
+    system("pause");
+}
---
+++ educational practice 2022/matrix determinant.cpp
@@ -0,0 +1,161 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
+
+void PrintMatr(int** mas, int m) {
+    int i, j;
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < m; j++)
+            cout << mas[i][j] << " ";
+        cout << endl;
+    }
+}
+
+void Minor(int** mas, int** p, int i, int j, int m) {
+    int ki, kj, di, dj;
+    di = 0;
+    for (ki = 0; ki < m - 1; ki++) {
+        if (ki == i) di = 1;
+        dj = 0;
+        for (kj = 0; kj < m - 1; kj++) {
+            if (kj == j) dj = 1;
+            p[ki][kj] = mas[ki + di][kj + dj];
+        }
+    }
+}
+
+int Opred(int** mas, int m, int k, int n1, int j) {
+    int i, d, t, n;
+    int** p;
+    p = new int* [m];
+    for (i = 0; i < m; i++)
+        p[i] = new int[m];
+    d = 0;
+    t = 1;
+    n = m - 1;
+    if (m < 1) cout << "Определитель вычислить невозможно!";
+    if (k < 0 || k > n1) cout << "Введен недопустимый номер столбца!";
+    if (k >= 0 && k <= n1) {
+        if (m == 1) {
+            d = mas[0][0];
+            return(d);
+        }
+
+        else if (m == 2) {
+            d = mas[1][1] * mas[0][0] - (mas[0][1] * mas[1][0]);
+            return(d);
+        }
+
+        else if (m > 2) {
+            for (i = 0; i < m; i++) {
+                if (k == n1) {
+                    Minor(mas, p, i, k, m);
+                    cout << "Минор " << n << "-го порядка для элелемента " << mas[i][j] << " имеет вид:\n";
+                    PrintMatr(p, n);
+                    if (k % 2 == 0) {
+                        if (n % 2 == 0)
+                            d = d + t * mas[i][j] * Opred(p, n, k, n1, j - 1);
+                        else
+                            d = d - t * mas[i][j] * Opred(p, n, k, n1, j - 1);
+                    }
+                    else {
+                        if (n % 2 == 0)
+                            d = d - t * mas[i][j] * Opred(p, n, k, n1, j - 1);
+                        else
+                            d = d + t * mas[i][j] * Opred(p, n, k, n1, j - 1);
+                    }
+                    t = -t;
+                }
+                else if (k == n1 - 1 && n1 > 4) {
+                    Minor(mas, p, i, k, m);
+                    cout << "Минор " << n << "-го порядка для элелемента " << mas[i][j] << " имеет вид:\n";
+                    PrintMatr(p, n);
+
+                    if (k % 2 == 0) {
+                        if (n > k)
+                            d = d - t * mas[i][j] * Opred(p, n, k, n1, j);
+                        else d = d + t * mas[i][j] * Opred(p, n, k, n1, j - 1);
+                    }
+                    else {
+                        if (n > k)
+                            d = d + t * mas[i][j] * Opred(p, n, k, n1, j);
+                        else
+                            d = d - t * mas[i][j] * Opred(p, n, k, n1, j - 1);
+                    }
+                    t = -t;
+                }
+                else {
+                    Minor(mas, p, i, k, m);
+                    cout << "Минор " << n << "-го порядка для элелемента " << mas[i][j] << " имеет вид:\n";
+                    PrintMatr(p, n);
+                    if (k % 2 == 0)
+                        d = d + t * mas[i][j] * Opred(p, n, k, n1, j);
+                    else d = d - t * mas[i][j] * Opred(p, n, k, n1, j);
+                    t = -t;
+                }
+            }
+            return(d);
+        }
+    }
+}
+
+
+int main()
+{
+    setlocale(0, "");
+    ifstream f;
+    int n, d, k;
+    int** a;
+    f.open("matr.txt");
+
+
+    cout << "Программа вычисления определителя методом разложения по k столбцу.\nСоздайте файл с именем matr.txt и укажите в нем размер матрицы, \n номер строки и значение самой матрицы через пробел.\n";
+        if (!f.is_open()) cout << "Файл не найден!\n";
+    if (f.is_open() && f.peek() == EOF) cout << "Файл пустой!\n";
+
+    if (f.is_open() && f.peek() != EOF) {
+        f >> n >> k;
+
+        if (f.fail()) cout << "Данные введены неверно!\n";
+
+        else {
+            a = new int* [n];
+            for (int i = 0; i < n; i++)
+                a[i] = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    f >> a[i][j];
+                }
+            }
+            if (f.fail()) cout << "Данные введены неверно!\n";
+            else {
+                cout << "Вы ввели матрицу:\n";
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+                        cout << a[i][j] << " ";
+                    }
+                    cout << endl;
+                }
+                cout << "Вы ввели столбец под номером " << k << endl << " \n";
+                k--;
+                int n1 = n - 1;
+                int j = k;
+                d = Opred(a, n, k, n1, j);
+
+                cout << "Определитель матрицы равен: " << d << endl;
+                f.close();
+                for (int i = 0; i < n; i++)
+                    delete a[i];
+                delete[]a;
+
+                ofstream t("opred.txt");
+                t << "Определитель матрицы равен: " << d << endl;
+                t.close();
+            }
+        }
+    }
+
+    system("pause");
+}
